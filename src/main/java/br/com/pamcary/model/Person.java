60290454f763d658a5119cfeebce15759fe3b608
Cleber Araujo <clebera@gmail.com>
2022-03-22 09:27:52 -0300

    add config and model

---
+++ src/main/java/br/com/pamcary/model/Person.java
@@ -0,0 +1,27 @@
+package br.com.pamcary.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "PESSO_FISICA")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO")
+    private long Id;
+    @Column(name = "NOME")
+    private String name;
+    @Column(name = "CPF")
+    private String cpf;
+    @Column(name = "DATA_NASCIMENTO")
+    private LocalDateTime birth;
+}
